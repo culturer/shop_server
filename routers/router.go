96da5fabe0d574eeeddbdb2000e560c34c2e1ea1
cultuler <78901214@qq.com>
2018-05-24 12:48:13 +0800

    修改过滤器

--- routers/router.go
+++ routers/router.go
@@ -48,18 +48,35 @@ func initRouter() {
 
 //初始化过滤器
 func initFilter() {
-	beego.InsertFilter("/*", beego.BeforeRouter, user_filter)
+	// beego.InsertFilter("/*", beego.BeforeRouter, user_filter)
 	//登录过滤器
 	beego.InsertFilter("/*", beego.BeforeRouter, login_filter)
 	//供应商采购页面过滤器
 	beego.InsertFilter("/procurement", beego.BeforeRouter, p_login_filter)
 }
 
+// func user_filter(ctx *context.Context) {
+// 	//过滤的url表
+// 	fileter_url := []string{"/login", "/register", "/products", "/get", "/wxhelper"}
+// 	//pid --- 分销商Id
+// 	for i := 0; i < len(fileter_url); i++ {
+// 		if ctx.Request.RequestURI == fileter_url[i] {
+// 			return
+// 		}
+// 	}
+// 	_, ok := ctx.Input.Session("uid").(int64)
+// 	if !ok {
+// 		//beego.Info(fmt.Sprintf("redirect,uid:%v", uid))
+// 		//ctx.Redirect(302, "/login")
+// 		ctx.Output.Body([]byte(`{"status":"302","msg":"请重新登陆"}`))
+// 	}
+// }
+
 //登录过滤器
 func login_filter(ctx *context.Context) {
 
 	//不过滤的url表
-	n_fileter_url := []string{"/login", "/register", "/products", "/get", "/procurement", "/p_login"}
+	n_fileter_url := []string{"/login", "/register", "/products", "/get", "/procurement", "/p_login", "/wxhelper"}
 
 	for i := 0; i < len(n_fileter_url); i++ {
 		if ctx.Request.RequestURI == n_fileter_url[i] {
@@ -91,20 +108,4 @@ func p_login_filter(ctx *context.Context) {
 		}
 	}
 
-}
-
-func user_filter(ctx *context.Context) {
-	//过滤的url表
-	fileter_url := []string{"/login", "/register", "/products", "/get", "/wxhelper"}
-	//pid --- 分销商Id
-	for i := 0; i < len(fileter_url); i++ {
-		if ctx.Request.RequestURI != fileter_url[i] {
-			_, ok := ctx.Input.Session("uid").(int64)
-			if !ok {
-				//beego.Info(fmt.Sprintf("redirect,uid:%v", uid))
-				//ctx.Redirect(302, "/login")
-				ctx.Output.Body([]byte(`{"status":"302","msg":"请重新登陆"}`))
-			}
-		}
-	}
 }
